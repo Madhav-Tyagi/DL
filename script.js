d4db3afc263639bc5826659b6ea148ac4e132f95
Madhav-Tyagi <145900335+Madhav-Tyagi@users.noreply.github.com>
2023-12-28 00:07:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var dm = document.querySelector("#dark");
+var lm = document.querySelector("Light");
+var bg = document.querySelector("body");
+
+function dark (){
+    bg.style.backgroundColor="grey";
+    bg.style.color="White";
+}
+
+function light (){
+    bg.style.backgroundColor="White";
+    bg.style.color="black";
+}
+
